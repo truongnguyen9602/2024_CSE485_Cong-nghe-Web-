c40d0c868fb229aefa9a73677bf1de1a735b3afa
truongnguyen9602 <truongnguyen9602@gmail.com>
2024-03-02 07:43:54 +0700

    project12

---
+++ Project12/assets/db.php
@@ -0,0 +1,13 @@
+<?php
+$navItems = [
+ "GIỚI THIỆU",
+ "TIN TỨC & THÔNG BÁO",
+ "TUYỂN SINH",
+ "ĐÀO TẠO",
+ "NGHIÊN CỨU",
+ "ĐỐI NGOẠI",
+ "VĂN BẢN",
+ "SINH VIÊN",
+ "LIÊN HỆ"
+];
+?>
---
+++ Project12/assets/index.php
@@ -0,0 +1,35 @@
+<?php
+include "db.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Project12</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link rel="stylesheet" href="css/prj12.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#"><i class="fas fa-home"></i></a>
+      </li>
+      <?php foreach ($navItems as $item): ?>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#"><?php echo $item; ?></a>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+</nav>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
